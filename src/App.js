65dd00d5a8bd88afdac1e8cfecfc072ad3b69c0e
pho7on <nikdu4@gmail.com>
2020-10-20 15:07:52 +0200

    prevState example

--- src/App.js
+++ src/App.js
@@ -1,47 +1,70 @@
 import Input from "./Input";
-import React from 'react';
-
+import React from "react";
 
 class App extends React.Component {
-  
   state = {
-    firstName:"",
-    lastName:"",
-    age:0,
-    list:[1,2,3,4]
-  }
-  listOne = [4,5,6,7];
-  
-  mergeArrays = ()=>{
-    this.setState(prevState=>({
-      list: [...prevState.list,...this.listOne]
+    firstName: "",
+    lastName: "",
+    age: 0,
+    list: [1, 2, 3, 4],
+    counter: 0
+  };
+  listOne = [4, 5, 6, 7];
+
+  mergeArrays = () => {
+    this.setState(prevState => {
+      return { list: [...prevState.list, ...this.listOne] };
+    });
+    this.setState(prevState => ({
+      list: [...prevState.list, ...this.listOne]
     }));
-  }
-  
-  handleInput= (name,value)=>{
-    this.setState({[name]:value});
-}
-  
+  };
+
+  handleInput = (name, value) => {
+    this.setState({ [name]: value });
+  };
+
+  handleIncrease = () => {
+    // this.setState({ counter: number + 1 });
+    this.setState(prevState => ({
+      counter: prevState.counter + 1
+    }));
+  };
+
   componentDidMount() {
-    this.handleInput();
+    this.mergeArrays();
   }
-  
-  render(){
+
+  render() {
     return (
       <div>
         <div className="inputs">
-          <Input handleInput={this.handleInput} value={this.state.firstName} name="firstName"/>
-          <Input handleInput={this.handleInput} value={this.state.lastName} name="lastName"/>
-          <Input handleInput={this.handleInput} value={this.state.age} name="age"/>
+          <Input
+            handleInput={this.handleInput}
+            value={this.state.firstName}
+            name="firstName"
+          />
+          <Input
+            handleInput={this.handleInput}
+            value={this.state.lastName}
+            name="lastName"
+          />
+          <Input
+            handleInput={this.handleInput}
+            value={this.state.age}
+            name="age"
+          />
         </div>
         <ul>
-          {this.state.list.map(e=>{
-            return <li>{e}</li>
+          {this.state.list.map(e => {
+            return <li>{e}</li>;
           })}
         </ul>
+        <div className="counter">
+          <button onClick={this.handleIncrease}>Increase</button>
+          <h2>{this.state.counter}</h2>
+        </div>
       </div>
-
-
     );
   }
 }
